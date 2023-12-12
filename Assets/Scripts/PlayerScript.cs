d18df83a202d34f5e424445135b914b3b9efc90b
NougatDepp <104033750+NougatDepp@users.noreply.github.com>
2023-12-12 02:45:24 +0100

    Controller Inputs and Artwork

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,59 +1,101 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Geschwindigkeit des Spielers
-    public float jumpForce = 10f; // Sprungkraft des Spielers
+    public float moveSpeed = 5f; 
+    public float jumpForce = 10f; 
 
     private Rigidbody2D rb;
+    private Animator anim;
     private bool isGrounded;
+    private SpriteRenderer spriteRen;
+
+    private PlayerControls controls;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        controls.Gameplay.Jump.performed += ctx => Jump();
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRen = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Gameplay.Disable();
     }
 
     void Update()
     {
-        // Überprüfen, ob der Spieler auf dem Boden steht
-        isGrounded = Physics2D.OverlapCircle(transform.position, 2f, LayerMask.GetMask("Ground"));
-
-        // Spieler springt, wenn die Leertaste gedrückt wird und er sich auf dem Boden befindet
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            Jump();
-        }
+        isGrounded = Physics2D.OverlapCircle(transform.position, 0.5f, LayerMask.GetMask("Ground"));
     }
 
     void FixedUpdate()
     {
-        // Spielerbewegung
+
         float horizontalInput = Input.GetAxis("Horizontal");
         Move(horizontalInput);
-
-        // Bodenprüfung für Animationen oder andere Funktionen
-        // Du könntest hier beispielsweise eine Animation auslösen, wenn der Spieler sich bewegt, etc.
+        
         if (isGrounded)
         {
-            // Hier könntest du weitere Funktionen auslösen, die nur dann aktiv sein sollen, wenn der Spieler am Boden ist.
+            anim.SetTrigger("Grounded");
         }
+        else
+        {
+            anim.ResetTrigger("Grounded");
+        }
+        
+        AnimationUpdate();
     }
 
     void Move(float horizontal)
     {
-        // Spielerbewegung
+
         Vector2 movement = new Vector2(horizontal * moveSpeed, rb.velocity.y);
         rb.velocity = movement;
     }
 
     void Jump()
     {
-        // Spieler springt
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            anim.SetTrigger("Jump");
+        }
+    }
+
+    void AnimationUpdate()
+    {
+        anim.SetFloat("AirSpeedY",rb.velocity.y);
+        if (Mathf.Floor(rb.velocity.x) > 0)
+        {
+            spriteRen.flipX = false;
+            anim.SetInteger("AnimState", 1);
+        }
+        else if (Mathf.Floor(rb.velocity.x) < 0)
+        {
+            spriteRen.flipX = true;
+            anim.SetInteger("AnimState", 1);
+        }
+        else
+        {
+            anim.SetInteger("AnimState", 2);
+        }
+
     }
 }
 
