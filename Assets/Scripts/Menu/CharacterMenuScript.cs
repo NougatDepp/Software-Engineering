7bd60bba55cebd3af42fc7fba9d58b0d17ad512c
LarsStiegmann <104630960+LarsStiegmann@users.noreply.github.com>
2024-01-15 18:59:27 +0100

    Character Select V2

--- Assets/Scripts/Menu/CharacterMenuScript.cs
+++ Assets/Scripts/Menu/CharacterMenuScript.cs
@@ -10,5 +10,74 @@ using Sequence = Unity.VisualScripting.Sequence;
 
 public class CharacterMenuScript : MonoBehaviour
 {
+    public static CharacterMenuScript instance;
 
+    public List<Character> characters = new List<Character>();
+
+    public GameObject charCellPrefab;
+    public Transform playerSlotsContainer;
+
+    public Character confirmedCharacter;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+
+        foreach (Character character in characters)
+        {
+            SpawnCharacterCell(character);
+        }
+    }
+
+    private void SpawnCharacterCell(Character character)
+    {
+        GameObject charCell = Instantiate(charCellPrefab, transform);
+
+        charCell.name = character.characterName;
+
+        Image artwork = charCell.transform.Find("Artwork").GetComponent<Image>();
+        TextMeshProUGUI name = charCell.transform.Find("NameRect").GetComponent<TextMeshProUGUI>();
+
+        artwork.sprite = character.characterSprite;
+        name.text = character.characterName;
+    }
+
+    public void ShowCharacterInSlot(int player, Character character)
+    {
+        bool nullChar = (character == null);
+
+        Color alpha = nullChar ? Color.clear : Color.white;
+        Sprite artwork = nullChar ? null : character.characterSprite;
+        string name = nullChar ? null : character.characterName;
+        string playernumber = "Player " + (player + 1);
+
+        Transform slot = playerSlotsContainer.GetChild(player);
+        Transform slotArtwork = slot.Find("Artwork");
+
+        DG.Tweening.Sequence s = DOTween.Sequence();
+        s.Append(slotArtwork.DOLocalMoveX(-300, .05f).SetEase(Ease.OutCubic));
+        s.AppendCallback(() => slotArtwork.GetComponent<Image>().sprite = artwork);
+        s.AppendCallback(() => slotArtwork.GetComponent<Image>().color = alpha);
+        s.Append(slotArtwork.DOLocalMoveX(300, 0));
+        s.Append(slotArtwork.DOLocalMoveX(0, .05f).SetEase(Ease.OutCubic));
+
+
+        slot.Find("Artwork").GetComponent<Image>().sprite = artwork;
+        slot.Find("PlayerX").GetComponent<TextMeshProUGUI>().text = playernumber;
+        slot.Find("CharName").GetComponent<TextMeshProUGUI>().text = name;
+    }
+
+    public void ConfirmCharacter(int player, Character character)
+    {
+        if (confirmedCharacter == null)
+        {
+            confirmedCharacter = character;
+            playerSlotsContainer.GetChild(player).DOPunchPosition(Vector3.down * 3, .3f, 10, 1);
+
+        }
+    }
 }
--- Assets/Scripts/Menu/CursorScript.cs
+++ Assets/Scripts/Menu/CursorScript.cs
@@ -11,7 +11,158 @@ using UnityEngine.UI;
 
 public class CursorScript : MonoBehaviour
 {
+    private InputActionAsset inputAsset;
+    private InputActionMap menu;
+    private InputAction move;
 
 
-    
+    //Cursor Detection
+    private GameObject ui_canvas;
+    private GraphicRaycaster gr;
+    private PointerEventData pointerEventData = new PointerEventData(null);
+
+
+    public Transform currentCharacter;
+    public bool ready;
+
+    public GameObject tokenPrefab;
+    public Transform token;
+    private bool hasToken;
+
+    private int id;
+
+    private void Awake()
+    {
+        inputAsset = gameObject.transform.parent.GetComponent<PlayerInput>().actions;
+        menu = inputAsset.FindActionMap("Menu");
+    }
+
+    private void Start()
+    {
+        CharacterMenuScript.instance.ShowCharacterInSlot(id, null);
+    }
+
+    private void OnEnable()
+    {
+
+        ui_canvas = GameObject.FindGameObjectWithTag("MenuCanvas");
+        gr = ui_canvas.GetComponent<GraphicRaycaster>();
+        token = Instantiate(tokenPrefab, transform.position, Quaternion.identity, GameObject.FindWithTag("MenuCanvas").transform).transform;
+        hasToken = true;
+
+        menu.FindAction("Choose").started += Choose;
+        menu.FindAction("Back").started += Back;
+        menu.FindAction("Go").started += Go;
+
+
+        move = inputAsset.FindAction("Move");
+    }
+
+    private void OnDisable()
+    {
+        menu.FindAction("Choose").started -= Choose;
+        menu.FindAction("Back").started -= Back;
+        menu.FindAction("Go").started -= Go;
+    }
+
+    private void FixedUpdate()
+    {
+        if (SceneManager.GetActiveScene().name != "CharacterSelect") gameObject.SetActive(false);
+        transform.position += new Vector3(move.ReadValue<Vector2>().x, move.ReadValue<Vector2>().y, 0) * 0.3f;
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(move.ReadValue<Vector2>().x, move.ReadValue<Vector2>().y, 0) * 0.3f * Time.deltaTime;
+
+        if (hasToken)
+        {
+            token.position = transform.position;
+        }
+
+        pointerEventData.position = Camera.main.WorldToScreenPoint(transform.position);
+        List<RaycastResult> results = new List<RaycastResult>();
+        gr.Raycast(pointerEventData, results);
+
+        if (hasToken)
+        {
+            if (results.Count > 0)
+            {
+                Transform raycastCharacter = results[0].gameObject.transform;
+
+                if (raycastCharacter != currentCharacter)
+                {
+                    SetCurrentCharacter(raycastCharacter);
+                }
+            }
+            else
+            {
+                if (currentCharacter != null)
+                {
+                    SetCurrentCharacter(null);
+                }
+            }
+        }
+    }
+
+    void SetCurrentCharacter(Transform t)
+    {
+        currentCharacter = t;
+
+        if (t != null)
+        {
+            int index = t.GetSiblingIndex();
+            Character character = CharacterMenuScript.instance.characters[index];
+            CharacterMenuScript.instance.ShowCharacterInSlot(id, character);
+        }
+        else
+        {
+            CharacterMenuScript.instance.ShowCharacterInSlot(id, null);
+        }
+    }
+
+    void TokenFollow(bool trigger)
+    {
+        hasToken = trigger;
+    }
+
+    private void SetChoosenCharacter()
+    {
+        gameObject.transform.parent.GetComponent<PlayerScript>()
+            .SetCharacter(CharacterMenuScript.instance.characters[currentCharacter.GetSiblingIndex()].character);
+    }
+
+    private void RemoveCharacter()
+    {
+        gameObject.transform.parent.GetComponent<PlayerScript>()
+            .SetCharacter(null);
+    }
+    public void SetID(int id)
+    {
+        this.id = id;
+    }
+
+    private void Go(InputAction.CallbackContext obj)
+    {
+        GameManager.instance.StartGame();
+    }
+
+    private void Back(InputAction.CallbackContext context)
+    {
+        CharacterMenuScript.instance.confirmedCharacter = null;
+        TokenFollow(true);
+        RemoveCharacter();
+        ready = false;
+    }
+
+    private void Choose(InputAction.CallbackContext context)
+    {
+        if (currentCharacter != null)
+        {
+            TokenFollow(false);
+            CharacterMenuScript.instance.ConfirmCharacter(id, CharacterMenuScript.instance.characters[currentCharacter.GetSiblingIndex()]);
+            SetChoosenCharacter();
+            ready = true;
+        }
+    }
 }
